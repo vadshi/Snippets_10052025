0106c931e4c839fee025e08d57a692f5013adb2f
Vadim Shikhovtsov <vadimx30@gmail.com>
2025-05-17 14:46:00 +0300

    Change standart options of SnippetForm

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,13 +1,29 @@
-from django.forms import ModelForm, ValidationError
+from django.forms import ModelForm, ValidationError, Textarea, TextInput
 from MainApp.models import Snippet
 
 
+# Описание возможностей по настройке форм
+# https://docs.djangoproject.com/en/dev/ref/forms/widgets/#django.forms.Widget.attrs
 
 class SnippetForm(ModelForm):
     class Meta:
         model = Snippet
         # Описываем поля, которые будем заполнять в форме
         fields = ['name', 'lang', 'code']
+        labels = {"name": "", "lang": "", "code": ""}
+        widgets = {
+            "name": TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Название сниппета",
+                "style": "max-width: 300px"
+            }),
+            "code": Textarea(attrs={
+                "placeholder": "Код сниппета",
+                "rows": 5,
+                "class": "input-large",
+                'style': 'width: 50% !important; resize: vertical !important;'
+            }),  
+        }
     
     def clean_name(self):
         """Метод для проверки длины поля <name>"""
